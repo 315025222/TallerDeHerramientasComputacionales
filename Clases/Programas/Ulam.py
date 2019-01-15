9e76cc732499848d7896a452f2018aca2fd06b9e
315025222 <akuxemilianox@ciencias.unam.mx>
2019-01-15 16:21:54 -0600

    Hicimos el algoritmo de Ulam y cuenta cuentas veces se repite

--- Clases/Programas/Ulam.py
+++ Clases/Programas/Ulam.py
@@ -4,14 +4,17 @@ def ulam(x):
         print("El número %g no es positivo o no es entero" % x)
     else:
         print(x)
+        i = 0
         while x > 1:
-            if x % 2 == 0:
+            if x % 2 == 0:  #se puede poner el contador en el mismo nivel del else y del if
                 x = x/2
+                i += 1
                 print(x)
             else:
                 x = 3*x + 1
+                i += 1
                 print(x)
+        print("El algoritmo se repitió " + str(i) + " veces")
 
 
-ulam(17)
-print(17.0.is_integer())
+ulam(1509)
