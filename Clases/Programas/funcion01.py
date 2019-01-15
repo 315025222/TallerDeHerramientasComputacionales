979b85bd6f9380b3390d928d2ec1bca3f6320977
315025222 <akuxemilianox@ciencias.unam.mx>
2019-01-15 14:06:10 -0600

    Hice una nueva función que calcula la raíz y dice cuantos ciclos hace

--- Clases/Programas/funcion01.py
+++ Clases/Programas/funcion01.py
@@ -5,8 +5,41 @@ def vabsoluto(x):
         return -x
 
 
-x = 10
-while x > 0:
-    x = x - 1
+z = 10
+while z > 0:
+    z = z - 1
 
-print(x)
+print(z)
+
+
+def raiz(x):
+    h = x
+    b = 1.0
+    e = 0.0001
+    while vabsoluto(b - h) > e:
+        h = (b + h)/2
+        b = x/h
+    return b
+
+
+print(raiz(9))
+print(raiz(4))
+print(raiz(1000000))
+
+
+def raiz1(x):
+    h = x
+    b = 1.0
+    e = 0.0001
+    z = 0
+    while vabsoluto(b - h) > e:
+        z += 1
+        h = (b + h)/2
+        b = x/h
+
+    return [x, b]
+
+
+lista = raiz1(9)
+
+print("Hice la operación " + str(lista[0]) + " veces y la raíz vale " + str(lista[1]))
