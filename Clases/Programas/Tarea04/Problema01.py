f44369842ebc0d63d09377d3628512c1277f3647
315025222 <akuxemilianox@ciencias.unam.mx>
2019-01-18 13:11:41 -0600

    Vimos nuevas bibliotecas e hicimos un archivo de latex

--- Clases/Programas/Tarea04/Problema01.py
+++ Clases/Programas/Tarea04/Problema01.py
@@ -1,4 +1,4 @@
-#Encontrar el mcd de dos números
+# Encontrar el mcd de dos números
 def divisores(x):
     if x == 0:
         return [1]
@@ -20,14 +20,20 @@ def mcd(x, y):
     longx = len(divx)
     longy = len(divy)
     lista = []
+    if x == 0 and y == 0:
+        return -1
+    elif x == 0:
+        return divy[0]
+    elif y == 0:
+        return divx[0]
+    else:
+        for i in range(0, longx):
+            for j in range(0, longy):
+                if divx[i] == divy[j]:
+                    lista.append(divx[i])
 
-    for i in range(0, longx):
-        for j in range(0, longy):
-            if divx[i] == divy[j]:
-                lista.append(divx[i])
-
-    lista.sort()
-    mayor = lista[len(lista) - 1]
-    return mayor
+        lista.sort()
+        mayor = lista[len(lista) - 1]
+        return mayor
 
 
--- Clases/Programas/Tarea04/Problema09.py
+++ Clases/Programas/Tarea04/Problema09.py
@@ -1,5 +1,61 @@
 i = 1
 while i < 11:
+    mult = i*1
+    print("1 por {} es igual a {}".format(i, mult))
+    i += 1
+
+i = 1
+while i < 11:
+    mult = i*2
+    print("2 por {} es igual a {}".format(i, mult))
+    i += 1
+
+i = 1
+while i < 11:
+    mult = i*3
+    print("3 por {} es igual a {}".format(i, mult))
+    i += 1
+
+i = 1
+while i < 11:
     mult = i*7
     print("7 por {} es igual a {}".format(i, mult))
     i += 1
+
+i = 1
+while i < 11:
+    mult = i*7
+    print("7 por {} es igual a {}".format(i, mult))
+    i += 1
+
+i = 1
+while i < 11:
+    mult = i*7
+    print("7 por {} es igual a {}".format(i, mult))
+    i += 1
+i = 1
+
+while i < 11:
+    mult = i*7
+    print("7 por {} es igual a {}".format(i, mult))
+    i += 1
+i = 1
+
+while i < 11:
+    mult = i*7
+    print("7 por {} es igual a {}".format(i, mult))
+    i += 1
+i = 1
+
+while i < 11:
+    mult = i*7
+    print("7 por {} es igual a {}".format(i, mult))
+    i += 1
+i = 1
+
+while i < 11:
+    mult = i*7
+    print("7 por {} es igual a {}".format(i, mult))
+    i += 1
+
+
